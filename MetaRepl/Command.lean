f25c1d3e465519931cc2a33b32ca3e3ce5611ac9
adamtopaz <github@adamtopaz.com>
2025-04-11 15:06:33 -0600

    basic meta code

--- MetaRepl/Command.lean
+++ MetaRepl/Command.lean
@@ -7,9 +7,12 @@ namespace MetaRepl
 
 structure Command (m : Type → Type) where
   description : Option String := none
-  paramsSchema : Json := json% { type : "object" }
-  outputSchema : Json := json% { type : "object" }
+  paramsSchema : Json := json% { type : ["object", "array", "string", "number", "boolean", "null"] }
+  outputSchema : Json := json% { type : ["object", "array", "string", "number", "boolean", "null"] }
   run (params : Json) : m Json
+
+def Command.liftM [MonadLiftT m n] (cmd : Command m) : Command n := 
+  { cmd with run params := cmd.run params }
 
 structure Commands (m : Type → Type) where
   data : Std.HashMap String <| Command m
@@ -48,5 +51,46 @@ initialize commandsExt :
   addEntryFn := fun m (a,b) => m.insert a b
   exportEntriesFn m := m.toArray
 }
+
+open Lean Elab Term 
+
+syntax (name := commandAttrStx) "command" ident : attr
+
+initialize registerBuiltinAttribute {
+  name := `commandAttrStx
+  descr := "Register a command"
+  add nm stx _ := match stx with 
+    | `(attr|command $id:ident) => Meta.MetaM.run' do 
+      let cinfo ← getConstInfo nm
+      let (_, _, body) ← Meta.forallMetaTelescope cinfo.type
+      let_expr Command _ := body | throwError "{nm} is not a valid command"
+      modifyEnv fun e => commandsExt.addEntry e (id.getId.toString, nm)
+    | _ => throwUnsupportedSyntax
+}
+
+def elabCommandForMonadViaLift (cmd m : Expr) : TermElabM Expr := do
+  Meta.mkAppOptM ``Command.liftM #[none, m, none, cmd]
+
+def elabCommandForMonadViaEval (cmd m : Expr) : TermElabM Expr := do
+  let (args, binders, body) ← Meta.forallMetaTelescope <| ← Meta.inferType cmd
+  let_expr Command n := body 
+    | throwError "{← Meta.ppExpr cmd} is not a function that outputs a command."
+  unless ← Meta.isDefEq m n do 
+    throwError "{← Meta.ppExpr n} is not defeq to {← Meta.ppExpr m}"
+  for (arg,binder) in args.zip binders do
+    if binder.isInstImplicit then 
+      arg.mvarId!.assign <| ← Meta.synthInstance <| ← Meta.inferType arg
+  instantiateMVars <| mkAppN cmd args
+
+def elabCommandForMonad (cmd m : Expr) : TermElabM Expr := do
+  try elabCommandForMonadViaLift cmd m
+  catch _ => try elabCommandForMonadViaEval cmd m
+  catch _ => throwError "Failed to elaborate {← Meta.ppExpr cmd} for monad {← Meta.ppExpr m}"
+
+def elabCommand (trigger : String) (m : Expr) : TermElabM Expr := do
+  let some declName := commandsExt.getState (← getEnv) |>.get? trigger
+    | throwError "Failed to find command with trigger {trigger}"
+  let c ← Meta.mkConstWithFreshMVarLevels declName
+  elabCommandForMonad c m
 
 end MetaRepl
---
+++ MetaRepl/Commands.lean
@@ -0,0 +1,25 @@
+import MetaRepl.Command
+
+namespace MetaRepl
+
+@[command ping]
+def pingPong [Monad m] : Command m where
+  description := "Reply with the input parameters" 
+  run := pure
+
+open Lean Elab Tactic in
+
+@[command tactic]
+def tactic : Command TacticM where
+  paramsSchema := json% { type : "string" }
+  description := "Eval a tactic"
+  run p := do 
+    match p.getStr? with
+    | .ok s => match Parser.runParserCategory (← getEnv) `tactic s with
+      | .ok tac => 
+        evalTactic tac
+        return .null
+      | .error e => throwError s!"Failed to parse parameter\n{s}\nas a tactic:\n{e}"
+    | .error e => throwError s!"Parameter\n{p}\nis not a string:\n{e}"
+
+end MetaRepl
