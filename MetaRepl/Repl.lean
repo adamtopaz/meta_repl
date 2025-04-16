b2bb0308d5a43dd5c904002e2cb2e5709171f42c
adamtopaz <github@adamtopaz.com>
2025-04-16 04:57:00 -0600

    Update repl

--- MetaRepl/Repl.lean
+++ MetaRepl/Repl.lean
@@ -13,19 +13,33 @@ inductive ReplErrorKind where
 
 structure ReplError (ε : Type) where
   kind : ReplErrorKind
+  idx? : Option Nat
   input? : Option Input
   error : ε
 
 structure Repl [STWorld w m] [MonadBacktrack σ m] [MonadExcept ε m] 
     (cmds : Commands m) where
+  /-- This runs before the main REPL loop. -/
   init : HistoryT Input Output m Unit
+  /-- This runs after the main REPL loop. -/
   term : HistoryT Input Output m Unit
+  /-- This checks whether to terminate the loop. -/
   finished : HistoryT Input Output m Bool
+  /-- Obtain the next state index and input for the REPL. -/
   getInput : HistoryT Input Output m (Nat × Input)
+  /-- Return an unknown command error. 
+    Parameters are the state index and input. -/
   unknownCmd : Nat → Input → HistoryT Input Output m ε
+  /-- Return an invalid index error. 
+    Parameters are the state index and input. -/
   invalidIdx : Nat → Input → HistoryT Input Output m ε
-  mkError : ReplErrorKind → ε → HistoryT Input Output m Error
-  sendOutput : Output → HistoryT Input Output m Unit
+  /-- The REPL tags errors with a kind and possibly an index and input.
+    This function is meant to use such a tagged error to create an 
+    error message to send. -/
+  mkError : ReplError ε → HistoryT Input Output m Error
+  /-- Send an output. 
+    Parameters are the state index at the output and the output itself. -/
+  sendOutput : Nat → Output → HistoryT Input Output m Unit
 
 partial
 def Repl.run 
@@ -37,18 +51,18 @@ def Repl.run
 where 
 step : ExceptT (ReplError ε) (HistoryT Input Output m) (Option Input × Result) := 
     commitIfNoEx do
-  let (idx, input) ← .adapt (.mk .failedInput none) <| .mk <| observing <| repl.getInput
-  let state : σ ← .adapt (.mk .invalidIdx input) <| .mk <| observing <| do 
+  let (idx, input) ← .adapt (.mk .failedInput none none) <| .mk <| observing <| repl.getInput
+  let state : σ ← .adapt (.mk .invalidIdx idx input) <| .mk <| observing <| do 
     let states := (← get).states
     match states[idx]? with 
     | some state => return state
     | none => throw <| ← repl.invalidIdx idx input
-  let cmd : Command m ← .adapt (.mk .unknownCmd input) <| .mk <| observing <| do 
+  let cmd : Command m ← .adapt (.mk .unknownCmd idx input) <| .mk <| observing <| do 
     match cmds.get input.method with
     | some cmd => return cmd
     | none => throw <| ← repl.unknownCmd idx input
   restoreState state
-  let out : Result ← .adapt (.mk .failedCmd input) <| .mk <| observing <| 
+  let out : Result ← .adapt (.mk .failedCmd idx input) <| .mk <| observing <| 
     cmd.run input.param
   return (input, out)
 loop : HistoryT Input Output m Unit := do 
@@ -56,11 +70,11 @@ loop : HistoryT Input Output m Unit := do
   match ← step with
   | .ok (inpt,res) => 
     recordHistory inpt (.result res) (← saveState)
-    repl.sendOutput <| .result res
-  | .error err => 
-    let error : Error ← repl.mkError err.kind err.error
+    repl.sendOutput (← get).head <| .result res
+  | .error err =>
+    let error : Error ← repl.mkError err
     recordHistory err.input? (.error error) none
-    repl.sendOutput <| .error error
+    repl.sendOutput (← get).head <| .error error
   loop
 go : HistoryT Input Output m Unit := do
   repl.init ; loop ; repl.term
