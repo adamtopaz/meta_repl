f6f39d003c66bb8a543f218ac36cbdae793567e6
adamtopaz <github@adamtopaz.com>
2025-04-22 07:01:37 -0600

    add close command

--- MetaRepl.lean
+++ MetaRepl.lean
@@ -2,3 +2,15 @@ import MetaRepl.Command
 import MetaRepl.History
 import MetaRepl.Repl
 import MetaRepl.Types
+
+namespace MetaRepl
+
+@[command close]
+def closeCmd [Monad m] [MonadState ReplSignal m] : Command m where
+  description := "Send the close signal to the REPL"
+  paramSchema := json% { type : "null" }
+  outputSchema := json% { type : "null" }
+  passive := true
+  run _ := do 
+    modify fun _ => .close
+    return ⟨.null⟩
